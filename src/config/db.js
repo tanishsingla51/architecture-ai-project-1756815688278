c6c8c9bb8ceb8310c194f16991586e05609933fb
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-09-02 17:51:36 +0530

    Add src/config/db.js

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`\nMongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("MONGODB connection error: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
